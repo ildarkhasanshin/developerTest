f8296879ecf23afb011a00e1dc3f10ae113be1ed
ildar r. khasanshin <0@10021987.ru>
2018-10-17 16:41:56 +0300

    Новостной сайт Lenta.ru отдает список новостей в формате rss по адресу https://lenta.ru/rss. Необходимо создать скрипт getLastNews.php, который при запуске из командной строки выведет последние 5 новостей в формате Название Ссылка на новость Анонс

---
+++ getLastNews.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * вывод последних пяти новостей с lenta.ru
+ * запуск из командной строки: php getLastNews.php
+ * author: ildar r. khasanshin .. 10021987.ru
+ */
+function file_get_contents_curl($href) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $href);
+	$data = curl_exec($ch);
+	curl_close($ch);
+
+	return $data;
+}
+
+$dat = file_get_contents_curl("https://lenta.ru/rss");
+if ( $dat && strlen($dat) > 0 ) {
+	$xml = new SimpleXMLElement($dat);
+
+	$count = 5;
+	for ( $i = 0; $i <= $count; $i ++ ) {
+		echo ($i + 1).') ';
+		echo $xml->channel->item[$i]->title."\r\n";
+		echo $xml->channel->item[$i]->guid."\r\n";
+		echo trim($xml->channel->item[$i]->description)."\r\n\r\n";
+	}
+}
